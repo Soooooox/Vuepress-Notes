2658bb9acc4bbbbda7591a0ff81f581795a3138b
Soooooox <150324926+Soooooox@users.noreply.github.com>
2025-07-12 02:55:05 +0000

    Deploying to docs_page from @ Soooooox/Vuepress-Notes@36a3054eaac32d91eb7960eda04e77098171a82a 🚀

--- src/.vuepress/sidebar.ts
+++
@@ -1,7 +0,0 @@
-import { sidebar } from "vuepress-theme-hope";
-
-import {getNestedSidebar } from './utils/autoSidebar.js'
-
-export default sidebar({
-    '/SecondBrain/': getNestedSidebar('SecondBrain')
-});
--- src/SecondBrain/README.md
+++
@@ -1,6 +0,0 @@
-<Catalog 
-  base="/SecondBrain/"
-  :hideHeading="true"
-  :level="2"
-  :index="false"
-/>
