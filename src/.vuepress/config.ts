b36513f830a3a59901a46de895e73f2eb395fd72
Soooooox <Q2820854831@gmail.com>
2025-07-12 10:27:56 +0800

    加入 base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Vuepress-Notes/",
 
   lang: "zh-CN",
   title: "Soooooox 的小屋",
